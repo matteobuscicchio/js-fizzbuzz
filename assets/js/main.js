98e0841122eebf16087d6fdd44863c5f655eac70
matteo <matteoezio@me.com>
2020-11-19 16:31:09 +0100

    migliorato il programma di calcolo

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,21 +12,33 @@
 
 var hundredNumber = [101];
 
-for (var i = 1; i < hundredNumber; i++) {
+for (var i = 1; i < 101; i++) {
 
     console.log(i);
     
-    if (i % 3 ===0) {
-
-        console.log('Fizz');
-
-    }else if (i % 5 ===0) {
-
-        console.log('Buzz');
-
-    } else if (i % 15 ===0) {
+    
+    if (i % 3 ===0 && i % 5 ===0) {
 
         console.log('FizzBuzz');
 
+    } else if (i % 3 ===0) {
+
+        console.log('Fizz');
+
+    } else if (i % 5 ===0) {
+
+        console.log('Buzz');
+
     }
+    
+
 }
+
+
+// var i = 0;
+// while (i<101) {
+
+//     console.log(i);
+
+//     i++
+// }
